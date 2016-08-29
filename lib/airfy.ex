059417eef4181ae0dd5ad19f717cfd889484e294
zaali kavelashvili <zaali@live.com>
2016-08-29 19:21:52 +0400

    make code prettier

--- lib/airfy.ex
+++ lib/airfy.ex
@@ -1,8 +1,7 @@
 defmodule Airfy do
   def encrypt(data) when is_binary(data) do
     case File.read("public_key") do
-      {:ok, content} ->
-        data |> RsaEx.encrypt(content)
+      {:ok, content} -> data |> RsaEx.encrypt(content)
       {:error, reason} -> {:error, reason}
       _ -> {:error, "Error while loading file"}
     end
