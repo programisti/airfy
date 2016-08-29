a7f96572b40281868313b5cf8596c02dd472c6c7
zaali kavelashvili <zaali@live.com>
2016-08-29 19:19:13 +0400

    add encrypt function to encrypt data via RsaEx deps

---
+++ lib/airfy.ex
@@ -0,0 +1,11 @@
+defmodule Airfy do
+  def encrypt(data) when is_binary(data) do
+    key = "123"
+    case File.read("public_key") do
+      {:ok, content} ->
+        data |> RsaEx.encrypt(content)
+      {:error, reason} -> {:error, reason}
+      _ -> {:error, "Error while loading file"}
+    end
+  end
+end
---
+++ test/airfy_test_test.exs
@@ -0,0 +1,8 @@
+defmodule AirfyTestTest do
+  use ExUnit.Case
+  doctest AirfyTest
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
